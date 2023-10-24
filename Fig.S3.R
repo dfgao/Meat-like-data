4661500487fbcdeb8b10700c5b4fcd8bf2ae6831
Dengfeng Gao <49668110+dfgao@users.noreply.github.com>
2023-10-24 19:06:50 +0800

    Update Fig.S3.R

--- Fig.S3.R
+++ Fig.S3.R
@@ -1,4 +1,4 @@
-# Fig.S1a --------
+# Fig.S3a --------
 pg.tpm_cor <- cor(pg.pef.tpm[,c(2:9)],method = 'pearson',use = 'pairwise.complete.ob')
 
 pheatmap(pg.tpm_cor,
@@ -10,3 +10,27 @@ pheatmap(pg.tpm_cor,
          annotation_col = ann,
          annotation_colors = ha_top.col,
          number_color = 'white')
+
+# Fig.S3b -------
+group_list_pg <- factor(c( rep('pgEpiSCs.Low',3), rep('pgEpiSCs.High',3),rep('PEF',2)),levels = c('pgEpiSCs.Low','pgEpiSCs.High','PEF'))
+
+d_p <- function(tpm,group_list){
+  tpm.t = as.data.frame(t(tpm))
+  tpm.t = cbind(tpm.t,group_list)
+  tpm.pca <- PCA(tpm.t[,-ncol(tpm.t)],graph = FALSE)
+  fviz_screeplot(tpm.pca, addlabels = TRUE, ylim = c(0, 70),title='Dim choose')
+  fviz_pca_ind(tpm.pca,
+               mean.point=F,
+               # repel = T,
+               label = "none",
+               geom.ind = c("point",'text'),
+               fill.ind = tpm.t$group_list,
+               palette = c(sample.col[1:2],'#e9c46a'),
+               legend.title = "Groups",
+               pointsize = 6,
+               pointshape = 21,
+               col.ind = "black",
+               title = 'Samples PCA'
+  ) + theme(axis.text = element_text(size = 12), axis.title = element_text(size = 15))
+}
+d_p(pg.pef.tpm[pick.genes,c(2:9)],group_list_pg)
